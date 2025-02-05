7b4938a42100cf492084d05d9ac35d472ac0d298
silvanamejia1 <SMEJIA2@ND.EDU>
2025-02-05 13:33:39 -0500

    OH today

---
+++ Computing II/basic-streamlit-app/main.py
@@ -0,0 +1,8 @@
+# Import the Streamlit library
+import streamlit as st
+import pandas as pd
+
+st.write("Hello, streamlit")
+st.markdown("This is a basic Streamlit app")
+st.write("This app displays a simple DataFrame as a demonstration.")
+
