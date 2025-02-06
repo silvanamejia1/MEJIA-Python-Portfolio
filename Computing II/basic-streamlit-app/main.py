c7dc30ac814232a02b198d9bade2d0090bbd11d7
silvanamejia1 <SMEJIA2@ND.EDU>
2025-02-06 12:06:58 -0500

    Update main.py

--- Computing II/basic-streamlit-app/main.py
+++ Computing II/basic-streamlit-app/main.py
@@ -11,7 +11,7 @@ st.write("This app displays a simple DataFrame as a demonstration.")
 if st.button("Click me to pick a color!"):
     color = st.color_picker("Pick a color", "#00f900")
 else:
-    st.write("Click me to view a Dataset!")
+    st.button("Click me to view a Dataset!"):
     st.subheader("Look at the Dataset Below!")
     #aca poner un boton que haga otra cosa (puede ser explore a data frame)
 
