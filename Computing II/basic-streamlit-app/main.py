687efb1f6205149ce3592667d3a7477efda67152
silvanamejia1 <SMEJIA2@ND.EDU>
2025-02-06 12:04:08 -0500

    Update main.py

--- Computing II/basic-streamlit-app/main.py
+++ Computing II/basic-streamlit-app/main.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 
 st.write("Hello, streamlit")
+st.title("Welcome!")
 st.markdown("This is a basic Streamlit app")
 st.write("This app displays a simple DataFrame as a demonstration.")
 
@@ -10,5 +11,7 @@ st.write("This app displays a simple DataFrame as a demonstration.")
 if st.button("Click me to pick a color!"):
     color = st.color_picker("Pick a color", "#00f900")
 else:
-    st.write("Please Click this Button...")
+    st.write("Click me to view a Dataset!"):
+    st.subheader("Look at the Dataset Below!")
+    #aca poner un boton que haga otra cosa (puede ser explore a data frame)
 
