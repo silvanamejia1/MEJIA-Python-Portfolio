e4b9dbdd5ad5b630cac3380f7e3d36aa9eda58bb
silvanamejia1 <SMEJIA2@ND.EDU>
2025-02-07 15:34:09 -0500

    portafolio update

--- Computing II/basic-streamlit-app/main.py
+++ Computing II/basic-streamlit-app/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 #Title Requirments
 st.title("Customer Shopping Trends Analysis")
-st.subheader("Explore customer shopping data to understand recent trends and filter by various parameters.")
+st.subheader("Explore Customer Shopping Data to understand Recent Trends and filter by Various Parameters.")
 
 #Data Load
 df = pd.read_csv("data/shopping_trends 2.csv")
@@ -20,11 +20,13 @@ st.write("Explore the data by maniputaing the filters below:")
 #Product Category Select box
 category = st.selectbox("Select a Product Category", df["Category"].unique())
 
+#Iten Selected multiselect
+items = st.multiselect("Select Items Purchased", df["Item Purchased"].unique())
+
 #Price Sldier 
 Price_Limit = st.slider("Select a Purchase Amount",\
     min_value=df["Amount(USD)"].min(),\
     max_value= df["Amount(USD)"].max())
-#Here how do I make it so that they can choose the min amount
 
 #City Select box
 State = st.selectbox("Select a Location", df["Location"].unique())
@@ -36,14 +38,17 @@ gender_selection = st.radio("Select Gender", df["Gender"].unique())
 
 #Filter Confirmation
 st.subheader('Selected Filters')
-st.write(f"Selected Category: {category}")
-st.write(f"Selected Price Limit (USD): ${Price_Limit}")
-st.write(f"Selected Location: {State}")
-st.write(f"Selected Gender: {gender_selection}")
+st.write("*Please note that The filtered data will only include the selected items within the specified category.*")
+st.write(f"**Selected Category:** {category}")
+st.write(f"**Selected Items:** {items}")
+st.write(f"**Selected Price Limit (USD):** ${Price_Limit}")
+st.write(f"**Selected Location:** {State}")
+st.write(f"**Selected Gender:** {gender_selection}")
+
 
 #Final Filtered Data
 st.subheader("Final Filtered Data")
 if st.button("Click here to confirm your filter selections and view your Final Dataset!"):
-    st.dataframe(df[(df["Category"] == category) & (df["Amount(USD)"] <= Price_Limit) & (df["Location"] == State) & (df["Gender"] == gender_selection)])
+    st.dataframe(df[(df["Category"] == category) & (df["Amount(USD)"] <= Price_Limit) & (df["Location"] == State) & (df["Gender"] == gender_selection) & (df["Item Purchased"].isin(items))])
 else:
     st.write("Feel free to return and change any of your selections!")
