508a341fe3adb3bac42d9b51e1223db172b79c16
silvanamejia1 <SMEJIA2@ND.EDU>
2025-04-09 22:20:30 -0400

    create clear_input function to clear inputs when necessary

--- Computing_2/NERStreamlitApp/NERStreamlitApp.py
+++ Computing_2/NERStreamlitApp/NERStreamlitApp.py
@@ -4,6 +4,7 @@ import pandas as pd
 #Import Streamlit
 import streamlit as st
 
+#Hi! My name is Silvana Mejia and I am a Junior majoring in Busienss Analytics at the University of Notre Dame. I want to work in Wealth Managment in the future.
 
 # Streamlit app 
 # Page Title
@@ -41,9 +42,14 @@ Add as many as you want!
 if "custom_patterns" not in st.session_state: 
     st.session_state.custom_patterns = [] 
 
+#I will define a clear_inputs() fucntion in order to make sure that text boxes are cleared when necessary
+def clear_inputs():
+    st.session_state.label_input = ""
+    st.session_state.pattern_input = ""
+
 #This is a text input box that allows the user to insert labels and patterns
-label = st.text_input("Entity Label (e.g. FRUIT, EVENT)") #Label
-pattern = st.text_input("Pattern to Match (e.g. apple, Bengal Bouts)") #pattern
+label = st.text_input("Entity Label (e.g. FRUIT, EVENT)", key = "label_input") #label_input key created to be able to reference later
+pattern = st.text_input("Pattern to Match (e.g. apple, Bengal Bouts)", key = "pattern_input") #pattern_input key created to be able to reference later
 
 #This if statement that adds new pattern to the custom_patterns list in the session or ask the user to add a pattern if text boxes left blank 
 if st.button("➕ Add Entity Pattern"): 
@@ -53,8 +59,7 @@ if st.button("➕ Add Entity Pattern"):
             "pattern": pattern.lower()
         })
         st.success(f"Pattern added Succesfuly: {label.upper()} → {pattern}") #success message displayed 
-        st.session_state.label_input = ""
-        st.session_state.pattern_input = ""
+        clear_inputs()
     else:
         st.warning("Please enter both a label and a pattern.") #warning message displayed because no new text was inputed
 
