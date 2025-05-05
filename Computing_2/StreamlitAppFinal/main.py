b9e893cb98c34e2ceb841a1dd35c7301e14da3c7
silvanamejia1 <SMEJIA2@ND.EDU>
2025-05-05 03:35:07 -0400

    fix instructions

--- Computing_2/StreamlitAppFinal/main.py
+++ Computing_2/StreamlitAppFinal/main.py
@@ -23,19 +23,10 @@ Use the filters on the left to personalize your plan and click the buttons to se
 """)
 
 # User Inputs Revelant to investment
-"- **Age** → Younger users are recommended to take more investment risk (more stocks). Older users shift toward safer options (more bonds)."
 age = st.slider("Your Age", 18, 80, 30)
-
-" - **Income** → Higher income allows for higher saving and investing percentages."
 income = st.number_input("Your Annual Income ($)", min_value=0, value=60000, step=1000)
-
-"- **Dependents** → More dependents means safer allocations (less stock, more bonds) and slightly higher essential expenses. This supposes taht a higher % of your income would go towards essential for otehr members in your family."
 dependents = st.slider("Number of Financial Dependents", 0, 5, 0)
-
-"- **Risk Tolerance** → If you prefer higher risk, your portfolio will lean more toward stocks. If lower, toward bonds. Answer this based on personal preference."
 risk_tolerance = st.selectbox("Risk Tolerance", ["Low", "Medium", "High"])
-
-"- **Savings Goal** → While not changing calculations directly, it helps frame your investment mindset. "
 goal = st.selectbox("Savings Goal", ["Retirement", "Education", "Buying a Home", "Other"])
 
 # Investemnt Calculations
